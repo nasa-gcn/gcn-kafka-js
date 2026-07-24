de330af0b3b167332f8c22432fc42475208c8214
Leo Singer <leo.singer@ligo.org>
2026-07-24 15:00:00 -0400

    Remove dependency on semver

--- prerelease.mjs
+++
@@ -1,5 +0,0 @@
-const {
-  default: { version },
-} = await import('./package.json', { with: { type: 'json' } })
-const { parse } = await import('semver')
-console.log(parse(version)?.prerelease[0] ?? '')
